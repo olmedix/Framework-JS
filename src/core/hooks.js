4be8e184512454e6b56090f131e8bd7e80cce924
olmedix <olmedix@gmail.com>
2025-11-28 15:19:26 +0100

    agregado UseEffect

--- src/core/hooks.js
+++ src/core/hooks.js
@@ -1,4 +1,3 @@
-// src/core/hooks.js
 import { getCurrentRoot } from "./renderer.js";
 
 export function useState(initialValue) {
@@ -21,4 +20,53 @@ export function useState(initialValue) {
   };
 
   return [root.hooks[index], setState];
+}
+
+export function useEffect(callback, deps) {
+  const root = getCurrentRoot();
+  if (!root) {
+    throw new Error("useEffect debe llamarse dentro de un componente renderizado");
+  }
+
+  const index = root.effectIndex++;
+  if (!root.effects[index]) {
+    // primera vez que se registra este efecto y lo prepara para ejecutarlo
+    root.effects[index] = {
+      callback,
+      deps,
+      cleanup: null,
+      shouldRun: true,
+    };
+    return;
+  }
+
+  const effect = root.effects[index];
+
+  // si no hay deps, siempre se ejecuta
+  /* useEffect(() => {
+      console.log("me ejecuto siempre");
+     }); */
+  if (!deps) {
+    effect.callback = callback;
+    effect.deps = undefined;
+    effect.shouldRun = true;
+    return;
+  }
+
+  // si hay deps, comprobamos si han cambiado
+  const prevDeps = effect.deps;
+  let changed = !prevDeps || prevDeps.length !== deps.length;
+
+  if (!changed) {
+    for (let i = 0; i < deps.length; i++) {
+      if (deps[i] !== prevDeps[i]) {
+        changed = true;
+        break;
+      }
+    }
+  }
+  // Marcamos si debe volver a ejecutarse.
+  effect.callback = callback;
+  effect.deps = deps;
+  effect.shouldRun = changed;
 }
--- src/core/renderer.js
+++ src/core/renderer.js
@@ -1,5 +1,3 @@
-// src/core/renderer.js
-
 let currentRoot = null;
 
 export function getCurrentRoot() {
@@ -13,11 +11,32 @@ export function createRoot(Component, container, initialProps = {}) {
     props: initialProps,
     hooks: [],
     hookIndex: 0,
+    effects: [],
+    effectIndex: 0,
   };
+
+  function runEffects() {
+    for (const effect of root.effects) {
+      // Saltamos los effects que no afectan a sus dependencias
+      if (!effect || !effect.shouldRun) continue;
+
+      // Si había un cleanup anterior, lo ejecutamos
+      if (typeof effect.cleanup === "function") {
+        effect.cleanup();
+      }
+      // Aquí se ejecuta realmente el useEffect
+      const cleanup = effect.callback();
+      // Guardamos el cleanup si el callback ha devuelto uno
+      effect.cleanup = typeof cleanup === "function" ? cleanup : undefined;
+      // Marcamos que el efecto ya se ejecutó
+      effect.shouldRun = false;
+    }
+  }
 
   function render() {
     currentRoot = root;
     root.hookIndex = 0;
+    root.effectIndex = 0;
 
     const vnode = root.Component(root.props || {});
     container.innerHTML = "";
@@ -27,6 +46,8 @@ export function createRoot(Component, container, initialProps = {}) {
     }
 
     currentRoot = null;
+
+    runEffects();
   }
 
   root.render = render;
